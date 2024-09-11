9926172f32450e8866e9fdbf1e82eec90276e4b2
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-09-10 21:37:07 -0300

    finalizando com api

--- src/components/ItemList/index.jsx
+++ src/components/ItemList/index.jsx
@@ -1,8 +1,11 @@
+import React from "react";
+import './styles.css';
 
-export default function index() {
-  return (
-    <div>
-      
-    </div>
-  )
+function ItemList({title, description}) {
+  return <div className="item-list">
+    <strong>{title}</strong>
+    <p>{description}</p>
+    <hr></hr>
+  </div>;
 }
+export default ItemList;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,27 @@
 import { Header } from '../../components/Header';
 import background from '../../assets/background.png';
 import './styles.css';
+import ItemList from '../../components/ItemList';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+const handleGetData = async () =>{
+  const userData = await fetch(`https://api.github.com/users/${user}`);
+  const newUser = await userData.json();
+  if(newUser.name){
+    const {avatar_url, name, bio, login} = newUser;
+    setCurrentUser({avatar_url, name,bio,login})
+    const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+    const newRepos = await reposData.json();
+    if(newRepos.length){
+      setRepos(newRepos);
+    }
+  }
+}
   return (
     <div className="App">
       <Header/> 
@@ -10,21 +29,31 @@ function App() {
         <img src={background} className="background" alt="background-app"/>
         <div className="info">
           <div>
-            <input name="usuario" placeholder="@username"/>
-            <button >Buscar</button>
+            <input name="usuario" value={user} onChange={(event) => setUser(event.target.value)} placeholder="@username"/>
+            <button onClick={handleGetData}>Buscar</button>
           </div>
-          <div className="perfil">
-            <img src="https://avatars.githubusercontent.com/u/145286206?s=400&u=633f35ac9a1c8850c3e568fdc49a91ebed19a4f0&v=4" alt="imagem-perfil" className="profile"/>
-            <div>
-              <h3>NicolasKonishi</h3>
-              <span>@NicolasKonishi</span>
-              <p>Descricao</p>
-            </div>
-          </div>  
+          {currentUser?.name ? (<>
+                <div className="perfil">
+                <img src={currentUser.avatar_url} alt="imagem-perfil" className="profile"/>
+                <div>
+                  <h3>{currentUser.name}</h3>
+                  <span>@{currentUser.login}</span>
+                  <p>{currentUser.bio}</p>
+                </div>
+              </div>  
           <hr/>
-          <div>
-            <h4>Repositórios</h4>
+
+         </> ) : null}
+          {repos?.length? (
+            <div>
+            <h4 className='repositorios'>Repositórios</h4>
+            {repos.map(repo => (
+
+              <ItemList title={repo.name} description={repo.description}/>
+            ))}
           </div>
+          ) : null}
+          
         </div>
       </div>
     </div>
